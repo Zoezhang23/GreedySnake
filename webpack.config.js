70343211c07194ba90d1727496345f82f4352538
Zoe Zhang <zhangzhijuan23@gmail.com>
2022-02-06 14:23:45 +0100

    first setup

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const { resolve } = require('path')
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        // environment: {
+        //     //老版本的IE浏览器兼容箭头函数
+        //     arrowFuntion: false
+        // }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+
+                use: [
+                    {
+                        //配置babel 加载
+                        loader: "babel-loader",
+                        //设置babel兼容的浏览器
+                        options: {
+                            presets: [
+                                [
+                                    //环境插件，自己去根据环境使用babel
+                                    "@babel/preset-env",
+                                    //配置信息,要兼容的目标浏览器
+                                    {
+                                        targets: {
+                                            chrome: "58"
+                                        },
+                                        //指定core版本
+                                        "corejs": "3",
+                                        //按需加载
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ],
+                        }
+                    },
+                    'ts-loader',
+                ],
+                exclude: /node_module/,
+            }]
+    },
+    //配置webpack插件
+    plugins: [
+        new CleanWebpackPlugin(),//每次打包都清除上一次打包的文件
+        new HTMLWebpackPlugin(
+            {
+                title: "Zoe Lover",
+                filename: "index.html",
+                //设置模板
+                template: "./src/index.html"
+            }
+        ),
+    ],
+    //设置引用的模块，可以引入文件import
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    target: 'node',
+    mode: 'production',
+    devServer: {
+        port: '8081',
+        open: true
+
+    }
+}
